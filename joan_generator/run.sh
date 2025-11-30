db6ffa6cc3f11e628a6bd25279cd7e2e416a096e
Adam7411 <101505511+Adam7411@users.noreply.github.com>
2025-11-30 09:18:10 +0100

    Enhance run.sh with error handling and logging

--- joan_generator/run.sh
+++ joan_generator/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "Startuje Joan Generator..."
-# Flaga -u jest KLUCZOWA do debugowania
-python3 -u /app/run.py
+echo "--> Startuje Joan Generator..."
+
+# Uruchamiamy pythona i przekierowujemy wszystkie błędy na ekran
+# Jeśli python padnie (||), wypisujemy komunikat i czekamy 10 minut
+python3 -u /app/run.py || { echo "!!! APLIKACJA PADŁA Z BŁĘDEM !!!"; sleep 600; }
